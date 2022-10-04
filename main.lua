b0e1369f6e28869ad88a1ebcc5238cd5f451a630
Kevin_Tung__ <kevin.tung@hytechtech.com>
2022-10-04 15:19:36 +0800

    + added fonts + time str might need adjustments

--- main.lua
+++ main.lua
@@ -1,10 +1,22 @@
 local x, y, secAngle, minAngle, hourAngle, timeStr, timeStrFont, background
 
 function getTimeStr(hour, min, sec)
-    local hs = hour < 10 and string.format('0%s', hour) or hour
-    local ms = min < 10 and string.format('0%s', min) or min
-    local ss = sec < 10 and string.format('0%s', sec) or sec
-    return string.format('%s : %s : %s', hs, ms, ss)
+    local hs = hour < 10 and string.format("0%s", hour) or hour
+    local ms = min < 10 and string.format("0%s", min) or min
+    local ss = sec < 10 and string.format("0%s", sec) or sec
+    return string.format("%s : %s : %s", hs, ms, ss)
+end
+
+function getWeekdayStr(wday)
+    t = {}
+    t[0] = "Sun"
+    t[1] = "Mon"
+    t[2] = "Tue"
+    t[3] = "Wed"
+    t[4] = "Thur"
+    t[5] = "Fri"
+    t[6] = "Sat"
+    return t[wday]
 end
 
 function love.load()
@@ -13,15 +25,16 @@ function love.load()
     secAngle = 0
     minAngle = 0
     hourAngle = 0
-    timeStrFont = love.graphics.newFont(30)
-    background = love.graphics.newImage('background.jpg')
+    timeStrFont = love.graphics.newFont("fonts/static/Orbitron-Regular.ttf", 30)
+    background = love.graphics.newImage("background.jpg")
 end
 
 function love.update(dt)
-    local time = os.date('*t')
+    local time = os.date("*t")
     local hour = time.hour
     local min = time.min
     local sec = time.sec
+    day = getWeekdayStr(time.wday)
     timeStr = getTimeStr(hour, min, sec)
 
     hourAngle = ((hour / 12) * math.pi * 2)
@@ -30,7 +43,7 @@ function love.update(dt)
 end
 
 function love.keypressed(key)
-    if key == 'escape' then
+    if key == "escape" then
         love.event.quit()
     end
 end
@@ -39,7 +52,7 @@ function drawRotateRect(x, y, width, height, angle)
     love.graphics.push()
     love.graphics.translate(x, y)
     love.graphics.rotate((-math.pi / 2) + angle)
-    love.graphics.rectangle('line', 0, 0, width, height)
+    love.graphics.rectangle("line", 0, 0, width, height)
     love.graphics.pop()
 end
 
@@ -51,5 +64,16 @@ function love.draw()
     drawRotateRect(x, y, 200, 4, minAngle)
     drawRotateRect(x, y, 100, 6, hourAngle)
     love.graphics.setColor(1, 1, 1)
-    love.graphics.print(timeStr, timeStrFont, love.graphics.getWidth() / 2 - (timeStrFont:getWidth(timeStr) / 2), love.graphics.getHeight() - timeStrFont:getHeight() - 10)
+    love.graphics.print(
+        timeStr,
+        timeStrFont,
+        love.graphics.getWidth() / 2 - (timeStrFont:getWidth(timeStr) / 2) - 100,
+        love.graphics.getHeight() - timeStrFont:getHeight() - 10
+    )
+    love.graphics.print(
+        day,
+        timeStrFont,
+        love.graphics.getWidth() / 2 - (timeStrFont:getWidth(timeStr) / 2) + 100,
+        love.graphics.getHeight() - timeStrFont:getHeight() - 10
+    )
 end
